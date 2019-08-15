b62d8301a6f9e8d57367dfb6aa38a830e6e2dac9
JoseOgam <jotienoochieng@gmail.com>
2019-08-15 10:18:08 +0300

    first commit

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Upload;
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('image');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        request()->validate([
+
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+
+        ]);
+
+
+
+        $file = time().'.'.request()->image->getClientOriginalExtension();
+
+
+
+        request()->image->move(public_path('images'), $file);
+
+
+
+        return back()
+
+            ->with('success','You have successfully upload image.')
+
+            ->with('image',$file);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Upload  $upload
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Upload $upload)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Upload  $upload
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Upload $upload)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Upload  $upload
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Upload $upload)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Upload  $upload
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Upload $upload)
+    {
+        //
+    }
+}
